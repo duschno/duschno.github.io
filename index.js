9bac558f529783e021817fbd6db4c4ff7eaf1d5f
duschno <36792393+duschno@users.noreply.github.com>
2023-09-26 13:22:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('lottie'), // Required
   path: 'https://raw.githubusercontent.com/duschno/duschno.github.io/main/data.json', // Required
-  renderer: 'svg', // Required
+  renderer: 'canvas', // Required
   loop: true, // Optional
   autoplay: true, // Optional
   name: "Hello World", // Name for future reference. Optional.
